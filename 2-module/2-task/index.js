be74b1b2023b42f925e0c2ad41da4d96303b6d6e
KirBoy <kirll.boy@yandex.ru>
2021-05-30 13:31:02 +0300

    2 dz

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -5,3 +5,21 @@ function isEmpty(obj) {
   return true
 }
 
+function sumSalary(salaries) {
+  let sum = 0
+
+  for (let key in salaries) {
+
+    if (typeof salaries[key] === 'number') {
+
+      if (salaries[key] == Infinity || salaries[key] == -Infinity || isNaN(salaries[key])) {
+      } else {
+        sum += salaries[key]
+      }
+    }
+  }
+
+  return sum
+}
+
+sumSalary(salaries)
--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -16,3 +16,29 @@ let calculator = {
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
+
+function isEmpty(obj) {
+  for (let key in obj) {
+    return false
+  }
+  return true
+}
+
+function sumSalary(salaries) {
+  let sum = 0
+
+  for (let key in salaries) {
+
+    if (typeof salaries[key] === 'number') {
+
+      if (salaries[key] == Infinity || salaries[key] == -Infinity || isNaN(salaries[key])) {
+      } else {
+        sum += salaries[key]
+      }
+    }
+  }
+
+  return sum
+}
+
+
