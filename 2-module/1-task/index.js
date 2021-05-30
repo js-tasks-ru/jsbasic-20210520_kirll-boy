2364a9cdb25f6044f2f9917aabbdbbd17bcd89db
KirBoy <kirll.boy@yandex.ru>
2021-05-30 12:51:24 +0300

    2 дз

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,18 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0
+
+  for (let key in salaries) {
+
+    if (typeof salaries[key] === 'number') {
+
+      if (salaries[key] == Infinity || salaries[key] == -Infinity || isNaN(salaries[key])) {
+      } else {
+        sum += salaries[key]
+      }
+    }
+  }
+
+  return sum
 }
+
+sumSalary(salaries)
