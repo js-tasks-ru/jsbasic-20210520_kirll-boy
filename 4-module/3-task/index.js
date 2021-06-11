194c5e0c5d003cb4e1d7a6b7ea60f0bde571272c
KirBoy <kirll.boy@yandex.ru>
2021-06-11 20:07:34 +0300

    4 dz

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,39 @@
 function highlight(table) {
-  // ваш код...
+  let tbody = table.querySelector('tbody')
+  let tds = tbody.querySelectorAll('td')
+  let trs = tbody.querySelectorAll('tr')
+
+
+  for (let tr of trs) {
+    for (let td of tds) {
+      let parent = td.closest('tr')
+
+      console.log(td)
+
+      if (td.textContent < 18) {
+        parent.style.textDecoration = 'line-through'
+      }
+
+      if (td.textContent == 'm') {
+        parent.classList.add('male')
+
+      } else if (td.textContent == 'f') {
+        parent.classList.add('female')
+      }
+
+      if (td.hasAttribute('data-available') == true) {
+
+        if (td.getAttribute('data-available') == 'true') {
+          parent.classList.add('available')
+
+        } else if (td.getAttribute('data-available') == 'false') {
+          parent.classList.add('unavailable')
+        }
+      }
+    }
+    
+    if ((tr.classList.contains('available') === false) && (tr.classList.contains('unavailable') === false)) {
+      tr.setAttribute('hidden', '')
+    }
+  }
 }
