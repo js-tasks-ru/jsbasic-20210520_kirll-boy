b894725f5fbef15f269397a29cc0cf911ba453be
KirBoy <kirll.boy@yandex.ru>
2021-06-30 19:58:46 +0300

    7 dz

--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -2,5 +2,71 @@ import createElement from '../../assets/lib/create-element.js';
 
 export default class Modal {
   constructor() {
+    this.elem = document.createElement('div')
+    this.elem.classList.add('modal')
+    this.open()
+    this.model()
+    this.buttonClose()
+    this.closeWithEscape()
+  }
+
+  model() {
+    this.elem.insertAdjacentHTML('beforeend', `
+    <div class="modal__overlay"></div>
+
+    <div class="modal__inner">
+      <div class="modal__header">
+    
+        <button type="button" class="modal__close">
+          <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
+        </button>
+
+        <h3 class="modal__title">
+          Вот сюда нужно добавлять заголовок
+        </h3>
+      </div>
+
+      <div class="modal__body">
+        A сюда нужно добавлять содержимое тела модального окна
+      </div>
+    </div>`)
+  }
+
+  open() {
+    const body = document.querySelector('body')
+    body.classList.add('is-modal-open')
+    body.append(this.elem)
+  }
+
+  setTitle(titleText) {
+    const title = this.elem.querySelector('.modal__title')
+    title.innerHTML = titleText
+  }
+
+  setBody(node) {
+    const modalBody = this.elem.querySelector('.modal__body');
+    modalBody.innerHTML = ''
+    console.log(node)
+    modalBody.append(node)
+  }
+
+  close() {
+    this.elem.remove()
+    document.body.classList.remove('is-modal-open')
+  }
+
+  buttonClose() {
+    const button = document.querySelector('.modal__close')
+    button.onclick = () => {
+      this.close()
+    }
+  }
+
+  closeWithEscape() {
+    document.onkeydown = (event) => {
+      if (event.code === 'Escape') {
+        this.close()
+      }
+    }
   }
 }
--- 7-module/3-task/index.js
+++ 7-module/3-task/index.js
@@ -1,4 +1,63 @@
 export default class StepSlider {
   constructor({ steps, value = 0 }) {
+    this.elem = document.createElement('div')
+    this.elem.classList.add('slider')
+    this.elem.addEventListener('click', ev => this.onClick(event, steps))
+
+    this.render(value, steps)
+    this.steps(steps, value)
   }
+
+  render(value, steps) {
+    let segments = steps - 1
+    let valuePercents = value / segments * 100
+    this.elem.insertAdjacentHTML('beforeend', ` 
+    <div class="slider__thumb" style="left: ${valuePercents}%">
+    <span class="slider__value">${value}</span>
+    </div> 
+    <div class="slider__progress" style=width:${valuePercents}%></div>`)
+  }
+
+
+  steps(steps, value) {
+    let i = 0
+    const slideSteps = document.createElement('div')
+    slideSteps.classList.add('slider__steps')
+    while (i < steps) {
+      slideSteps.insertAdjacentHTML('beforeend', `<span></span>`)
+      i++
+    }
+    slideSteps.childNodes[value].classList.add('slider__step-active')
+    this.elem.append(slideSteps)
+  }
+
+  onClick(event, steps) {
+    let left = event.clientX - this.elem.getBoundingClientRect().left
+    let leftRelative = left / this.elem.offsetWidth
+    let segments = steps - 1
+    let approximateValue = leftRelative * segments
+    let value = Math.round(approximateValue)
+    let valuePercents = value / segments * 100
+
+    const thumb = document.querySelector('.slider__thumb')
+    const progress = document.querySelector('.slider__progress')
+    const sliderValue = document.querySelector('.slider__value')
+    let sliderSteps = document.querySelector('.slider__steps')
+
+    sliderSteps = sliderSteps.querySelectorAll('span');
+
+    for (let elem of sliderSteps) {
+      elem.classList.remove("slider__step-active");
+    }
+    sliderSteps[value].classList.add("slider__step-active");
+
+    sliderValue.textContent = `${value}`
+    thumb.style.left = `${valuePercents}%`;
+    progress.style.width = `${valuePercents}%`;
+    this.elem.dispatchEvent(new CustomEvent('slider-change', {
+      detail: value,
+      bubbles: true
+    }));
+  }
+
 }
